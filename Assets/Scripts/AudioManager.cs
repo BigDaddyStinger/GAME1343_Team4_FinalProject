ad1e36d83997e69ed20a1e5b025ef351d4aba2c4
Blackcat-gif <157761600+Blackcat-gif@users.noreply.github.com>
2025-05-07 21:55:35 -0500

    Add files via upload

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
